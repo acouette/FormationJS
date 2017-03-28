1b919ea5d205c1b584cae7cfe7e50bf8325edd6f
Antoine Couette <ace@diginext.fr>
2017-03-28 18:16:31 +0200

    work in progress

---
+++ coding1/index.js
@@ -0,0 +1,24 @@
+for (var i = 0; i < 4; i++) {
+  console.log(i);
+}
+
+var j = 0;
+while (j < 4) {
+  console.log(j++);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(2, 3));
+
+
+var kid = {
+  name: 'marie',
+  age: 2
+};
+
+
+
+console.log(kid.name);
